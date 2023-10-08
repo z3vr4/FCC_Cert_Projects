d776e984c1c927bc82d09858f747986e7d451156
Alexis Garcia Del Rio <144841706+z3vr4@users.noreply.github.com>
2023-10-08 21:55:14 +0200

    Update Mean_Variance_Standard_Deviation_Calculator.py
    Added comentary on the purpose/workings on the code

--- Mean_Variance_Standard_Deviation_Calculator.py
+++ Mean_Variance_Standard_Deviation_Calculator.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+# TLDR of what this had to do:
+# Take an input (such as [0,1,2,3,4,5,6,7,8] )
+# Turn it into a matrix and return a dictionary like this:
+#{
+#  'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
+#  'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
+#  'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
+#  'max': [[6, 7, 8], [2, 5, 8], 8],
+#  'min': [[0, 1, 2], [0, 3, 6], 0],
+#  'sum': [[9, 12, 15], [3, 12, 21], 36]
+#}
+#
+
+
 def calculate(list):
   # Aarr - plain list || Barr - 2d || Carr - transposed Barr || Darr - All arrays
   Aarr = list
